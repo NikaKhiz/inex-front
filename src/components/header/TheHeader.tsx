c3fcc7c2f3bcdf5a434059ba01a9aa8fd9baa7c0
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-21 22:32:02 +0400

    refactor: header

--- src/components/header/TheHeader.tsx
+++ src/components/header/TheHeader.tsx
@@ -1,4 +1,5 @@
 import { HiMenu } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 import { LanguageDropdown } from 'src/components';
 import { Button } from 'src/components';
 const TheHeader = () => {
@@ -7,10 +8,14 @@ const TheHeader = () => {
       <p className='capitalize text-2xl'>Some awesome logo</p>
       <div className='flex items-center justify-between gap-4'>
         <LanguageDropdown />
-        <div className='hidden md:flex items-center gap-4'>
-          <Button type='filled'>sign up</Button>
-          <Button type='outline'>log in</Button>
-        </div>
+        <nav className='hidden md:flex items-center gap-4'>
+          <Link to='register'>
+            <Button type='filled'>sign up</Button>
+          </Link>
+          <Link to='login'>
+            <Button type='outline'>log in</Button>
+          </Link>
+        </nav>
         <button className='w-6 h-6 block md:hidden'>
           <HiMenu className='block w-full h-full bg-cover' />
         </button>
