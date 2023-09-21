fb2bbe77dac584e22d53534d67ad94a79cf76d4e
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-21 22:03:20 +0400

    fix: footer classname typo

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,7 +1,7 @@
 const currentDate = new Date().getFullYear();
 const TheFooter = () => {
   return (
-    <footer className='absolute bottom-0 w-full px-4 py-2 md:px-8 md:py-4 bg-darkBlack text-primaryBrown'>
+    <footer className='absolute bottom-0 w-full px-4 py-2 md:px-8 md:py-4 bg-primaryBlack text-primaryBrown'>
       <p className='capitalize text-sm font-medium'>
         &copy; {currentDate} Inexgroup. all rights reserved.
       </p>
