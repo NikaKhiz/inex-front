3c17fd9b7120f5de8becee3739647c865b9c1b60
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-21 14:37:00 +0400

    style: footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primaryWhite: '#FFFFFF',
+        primaryRed: '#E31221',
+        primaryBrown: '#DDCCAA',
+        primaryBlack: '#11101A',
+      },
+      colors: {
+        primaryWhite: '#FFFFFF',
+        primaryRed: '#E31221',
+        primaryBlack: '#11101A',
+      },
+      backgroundImage: () => ({
+        gradientPrimary:
+          'linear-gradient(187.16deg,#181623 0.07%,#191725 51.65%,#0D0B14 98.75%)',
+      }),
+    },
   },
   plugins: [],
 };
