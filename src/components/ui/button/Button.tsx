1a22c8eee30a152c692294270cb162484fb9b701
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-22 18:42:52 +0400

    style: sidebar also add sidebar and language box toggling functionalities

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -3,7 +3,7 @@ import { PropsType } from './types';
 const Button = (props: PropsType) => {
   return (
     <button
-      className={`capitalize font-normal px-6 py-1 text-primaryWhite ${
+      className={`w-full capitalize font-normal px-6 py-1 text-primaryWhite ${
         props.type === 'filled' ? 'bg-primaryRed' : 'bg-transparent'
       } border ${
         props.type === 'filled' ? 'border-primaryRed' : 'border-primaryWhite'
