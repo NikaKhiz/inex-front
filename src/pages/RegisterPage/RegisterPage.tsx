64a59a2468b6705a939b150e86521d09db197ae2
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-24 00:38:56 +0400

    feat: register users add registering functionality and handle server side errors

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,6 @@
 import { TheWrapper } from 'src/components';
 import { Button } from 'src/components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { TheForm } from 'src/components';
 import { FormHeading } from 'src/components';
@@ -8,12 +8,15 @@ import { FormGroup } from 'src/components/formgroup';
 import { FormData } from './types';
 import { singupFormValidationSchema } from 'src/schemas';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { signUp } from 'src/services';
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
   const {
     register,
     formState: { errors },
     handleSubmit,
+    setError,
   } = useForm<FormData>({
     defaultValues: {
       username: '',
@@ -25,7 +28,20 @@ const RegisterPage = () => {
   });
 
   const onSubmit = (data: FormData) => {
-    console.log('submitted', data);
+    signUp(data)
+      .then(() => navigate('/'))
+      .catch((error) => {
+        const errorsObj = error.response.data.errors;
+        for (const fieldName in errorsObj) {
+          if (Object.prototype.hasOwnProperty.call(errorsObj, fieldName)) {
+            const errorMessage = errorsObj[fieldName];
+            setError(fieldName as keyof FormData, {
+              type: 'message',
+              message: errorMessage,
+            });
+          }
+        }
+      });
   };
 
   return (
