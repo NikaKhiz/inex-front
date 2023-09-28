73b6781683ef123e8babd41820620e5f5debd004
NikaKhiz <khizambarelinika123@gmail.com>
2023-09-29 02:35:45 +0400

    feat: localize dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import { TheWrapper } from 'src/components';
 import { useUserState } from 'src/state';
 
 const Dashboard = () => {
   const username = useUserState((state) => state.username);
   const email = useUserState((state) => state.email);
+  const { t } = useTranslation();
+
   return (
     <TheWrapper>
       <div className='bg-neutralBlack  flex flex-col gap-4 p-4 text-primaryBrown  text-center text-xl md:text-3xl capitalize '>
         <p>
-          welcome, <span className='text-darkBlue'>{username}</span>
+          {t('dashboard.welcome')},{' '}
+          <span className='text-darkBlue'>{username}</span>
         </p>
         <p>
-          your current email adress is :{' '}
+          {t('dashboard.current_email')}{' '}
           <span className='text-darkBlue'>{email}</span>
         </p>
       </div>
